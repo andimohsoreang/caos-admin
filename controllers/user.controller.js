8edd6f3d13ef41c9e2284e680297104d541ae9b3
fachiri <fachrycooles@gmail.com>
2022-11-28 05:44:25 +0800

    instalasi sequelize

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,47 +1,44 @@
 const bcrypt = require('bcryptjs')
-const uuid = require('uuid')
-const db = require('../config/db.js')
+const model = require('../models/index')
 
 module.exports = {
-    userStore: (req, res) => {
-        db.query(`SELECT * FROM users WHERE email = '${req.body.email}'`, (err, result) => {
-            if (typeof result != 'undefined') {
-                // check email
-                if (result.length) {
-                    return res.status(409).send({
-                        status: 'Failed',
-                      message: 'This email is already in use!'
-                    })
-                }
-            }
-            // encrypt password
-            bcrypt.hash(req.body.password, 10, (err, hash) => {
+    userStore: async (req, res) => {
+        // cek email
+        const userData = await model.User.findOne({ where: { email: req.body.email } })
+        if (userData === null) {
+            // enkripsi password
+            let password = null
+            bcrypt.hash(req.body.password, 10, async (err, hash) => {
                 if (err) {
                     return res.status(500).send({
-                    status: 'Failed',
-                    message: err
+                        status: 'Failed',
+                        message: err
                     })
                 }
-                // insert user
-                db.query(`INSERT INTO users (uuid, name, email, password) VALUES ('${uuid.v4()}', '${req.body.name}', '${req.body.email}', '${hash}')`, (err, result) => {
-                    if (err) {
-                        return res.status(400).send({
-                            status: 'Failed',
-                            message: err
-                        })
-                    }
-                    return res.status(201).send({
-                        status: 'success',
+                // buat user baru
+                await model.User.create({ 
+                    name: req.body.name, 
+                    email: req.body.email,
+                    password: hash
+                }).then((result) => {
+                    res.status(200).send({
+                        status: 'Success',
                         message: 'Registered!',
-                        data: {
-                            uuid: req.body.uuid,
-                            name: req.body.name,
-                            email: req.body.email,
-                            password: hash
-                        }
+                        data: result 
+                    })
+                }).catch((err) => {
+                    console.log(err)
+                    res.status(500).send({
+                        status: 'failed',
+                        message: err
                     })
                 })
             })
-        })
+        } else {
+            res.status(409).send({
+                status: 'Failed',
+                message: 'This email is already in use!'
+            })
+        }
     }
 }
---
+++ migrations/20221127181713-create-user.js
@@ -0,0 +1,41 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      uuid: {
+        allowNull: false,
+        type: Sequelize.UUID
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING(50)
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING(50)
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  }
+};
