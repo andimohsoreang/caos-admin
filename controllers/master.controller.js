561ac29fb5e71b0b5bc67e9fe9e4ad3814563e7d
fzrsahi <fazrul.anugrah17@gmail.com>
2022-12-09 14:45:15 +0800

    add feature CRUD users

--- controllers/master.controller.js
+++ controllers/master.controller.js
@@ -1,30 +1,193 @@
-const model = require('../models/index')
+const bcrypt = require("bcryptjs");
+const model = require("../models/index");
 module.exports = {
-    users: async (req, res) => {
-        const data = await model.User.findAll({
-            attributes: ['name', 'email', 'role', 'status']
+  users: async (req, res) => {
+    const data = await model.User.findAll({
+      attributes: ["uuid", "name", "email", "role", "status"],
+    });
+    res.render("./pages/users", { data });
+  },
+  storeUsers: async (req, res) => {
+    let { name, email, role, password } = req.body;
+
+    if (password === null) password = /(\w+)@/g.exec(email)[1];
+
+    bcrypt.hash(password, 10, async (err, hash) => {
+      if (err) {
+        console.log("hasdsadalo");
+        return req.flash("alert", {
+          hex: "#f3616d",
+          color: "danger",
+          status: "Failed",
+        });
+      }
+      await model.User.create({
+        name: name,
+        email: email,
+        role: role,
+        password: hash,
+      })
+        .then((result) => {
+          req.flash("alert", {
+            hex: "#28ab55",
+            color: "success",
+            status: "Success",
+          });
+          req.flash("message", `User baru berhasil ditambahkan`);
+          res.status(201);
         })
-        res.render('./pages/users', { data })
-    },
-    categories: async (req, res) => {
-        const data = await model.Category.findAll({
-            attributes: ['name', 'description']
-        })
-        res.render('./pages/categories', { data })
-    },
-    storecategory: async (req, res) => {
-        const { name, description } = req.body
-        await model.Category.create({
-            name, description
-        }).then(() => {
-            req.flash('alert', {hex: '#28ab55', color: 'success', status: 'Success'})
-            req.flash('message', 'Kategori berhasil ditambahkan')
-            res.redirect('/categories')
-        }).catch(() => {
-            req.flash('alert', {hex: '#f3616d', color: 'danger', status: 'Failed'})
-            req.flash('message', 'Gagal menambahkan kategori')
-            res.redirect('/categories')
-        })
-        
+        .catch((result) => {
+          console.log(result);
+          req.flash("alert", {
+            hex: "#f3616d",
+            color: "danger",
+            status: "Gagal Menambahkan users baru",
+          });
+        });
+      res.redirect("/users");
+    });
+  },
+  updateUser: async (req, res) => {
+    let { name, email, role, password } = req.body;
+    const response = await model.User.findOne({
+      where: {
+        uuid: req.params.uuid,
+      },
+    });
+    if (!response) {
+      req.flash("alert", {
+        hex: "#f3616d",
+        color: "danger",
+        status: "User tidak ditemukan",
+      });
+      res.redirect("/users");
     }
-}
+    if (!password) {
+      await model.User.update(
+        {
+          name,
+          email,
+          role,
+          password: response.password,
+        },
+        {
+          where: {
+            uuid: req.params.uuid,
+          },
+        }
+      )
+        .then((result) => {
+          req.flash("alert", {
+            hex: "#28ab55",
+            color: "success",
+            status: "Success",
+          });
+          req.flash("message", `Berhasil Update User`);
+          res.status(201);
+          res.redirect("/users");
+        })
+        .catch((result) => {
+          console.log(result);
+          req.flash("alert", {
+            hex: "#f3616d",
+            color: "danger",
+            status: "Gagal Update users baru",
+          });
+          res.redirect("/users");
+        });
+    } else {
+      bcrypt.hash(password, 10, async (err, hash) => {
+        if (err) {
+          return req.flash("alert", {
+            hex: "#f3616d",
+            color: "danger",
+            status: "Failed",
+          });
+        }
+        await model.User.update(
+          {
+            name,
+            email,
+            role,
+            password: hash,
+          },
+          {
+            where: {
+              uuid: req.params.uuid,
+            },
+          }
+        )
+          .then((result) => {
+            console.log("then");
+            req.flash("alert", {
+              hex: "#28ab55",
+              color: "success",
+              status: "Success",
+            });
+            req.flash("message", `Berhasil Update User`);
+            res.status(201);
+            res.redirect("/users");
+          })
+          .catch((result) => {
+            console.log("catch");
+            console.log(result);
+            req.flash("alert", {
+              hex: "#f3616d",
+              color: "danger",
+              status: "Gagal Update users baru",
+            });
+            res.redirect("/users");
+          });
+      });
+    }
+  },
+  deleteUser: async (req, res) => {
+    const response = await model.User.findOne({
+      where: {
+        uuid: req.params.uuid,
+      },
+    });
+    if (!response) {
+      req.flash("alert", {
+        hex: "#f3616d",
+        color: "danger",
+        status: "User tidak ditemukan",
+      });
+      res.redirect("/users");
+    }
+
+    // await model.User.
+
+  },
+  categories: async (req, res) => {
+    const data = await model.Category.findAll({
+      attributes: ["name", "description"],
+    });
+    res.render("./pages/categories", { data });
+  },
+  storecategory: async (req, res) => {
+    const { name, description } = req.body;
+    await model.Category.create({
+      name,
+      description,
+    })
+      .then(() => {
+        req.flash("alert", {
+          hex: "#28ab55",
+          color: "success",
+          status: "Success",
+        });
+        req.flash("message", "Kategori berhasil ditambahkan");
+        res.redirect("/categories");
+      })
+      .catch(() => {
+        req.flash("alert", {
+          hex: "#f3616d",
+          color: "danger",
+          status: "Failed",
+        });
+        req.flash("message", "Gagal menambahkan kategori");
+        res.redirect("/categories");
+      });
+  },
+};
--- routes/web.js
+++ routes/web.js
@@ -1,43 +1,48 @@
-const express = require('express')
-const router = express.Router()
-const authController = require('../controllers/auth.controller')
-const uploadController = require('../controllers/upload.controller')
-const adminController = require('../controllers/admin.controller')
-const articleController = require('../controllers/article.controller')
-const masterController = require('../controllers/master.controller')
-const verifyUser = require('../middlewares/verify')
-const multipart = require('connect-multiparty');
+const express = require("express");
+const router = express.Router();
+const authController = require("../controllers/auth.controller");
+const uploadController = require("../controllers/upload.controller");
+const adminController = require("../controllers/admin.controller");
+const articleController = require("../controllers/article.controller");
+const masterController = require("../controllers/master.controller");
+const verifyUser = require("../middlewares/verify");
+const multipart = require("connect-multiparty");
 const multipartMiddleware = multipart();
-const userMiddleware = require('../middlewares/user.middleware');
-
-
+const userMiddleware = require("../middlewares/user.middleware");
 
 // -pages
-router.get('/insertarticle',articleController.article);
-router.get('/getarticle',articleController.getarticle);
+router.get("/insertarticle", articleController.article);
+router.get("/getarticle", articleController.getarticle);
 // --auth
-router.get('/login', verifyUser.loggedIn, authController.loginPage)
-router.get('/register', verifyUser.loggedIn, authController.registerPage)
+router.get("/login", verifyUser.loggedIn, authController.loginPage);
+router.get("/register", verifyUser.loggedIn, authController.registerPage);
 // --admin
-router.get('/', verifyUser.isLogin, adminController.dashboard)
+router.get("/", verifyUser.isLogin, adminController.dashboard);
 // ---master
-router.get('/users', masterController.users)
-router.get('/categories', masterController.categories)
+router.get("/users", masterController.users);
+router.get("/categories", masterController.categories);
 // ---algorithm
-router.get('/importdataset', adminController.importdataset)
-router.get('/dataprocessing', adminController.dataprocessing)
-router.get('/performance', adminController.performance)
-router.get('/dataprediction', adminController.dataprediction)
-router.get('/resultprediction', adminController.resultprediction)
+router.get("/importdataset", adminController.importdataset);
+router.get("/dataprocessing", adminController.dataprocessing);
+router.get("/performance", adminController.performance);
+router.get("/dataprediction", adminController.dataprediction);
+router.get("/resultprediction", adminController.resultprediction);
 
 // process
-router.post('/auth/login', authController.login)
-router.post('/auth/register', authController.register)
-router.get('/logout', authController.logout)
-router.post('/uploaddataset', multipartMiddleware, uploadController.dataset)
-router.post('/processperformance', adminController.processperformance)
-router.post('/processprediction', adminController.processprediction)
-router.post('/insertarticle', userMiddleware.validateImages,articleController.insertarticle)
-router.post('/category/store', masterController.storecategory)
+router.post("/auth/login", authController.login);
+router.post("/auth/register", authController.register);
+router.get("/logout", authController.logout);
+router.post("/uploaddataset", multipartMiddleware, uploadController.dataset);
+router.post("/processperformance", adminController.processperformance);
+router.post("/processprediction", adminController.processprediction);
+router.post(
+  "/insertarticle",
+  userMiddleware.validateImages,
+  articleController.insertarticle
+);
+router.post("/category/store", masterController.storecategory);
+router.post("/users/store", masterController.storeUsers);
+router.post("/users/update/:uuid", masterController.updateUser);
+router.get("/users/delete/:uuid", masterController.deleteUser);
 
-module.exports = router
+module.exports = router;
